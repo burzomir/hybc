a12831b117252e9b7da5aca5f4944e8628e43262
Jędrzej Rosłaniec <jedr.ros@gmail.com>
2017-10-29 11:37:45 +0100

    Change device id regex

--- backend/app/routing.py
+++ backend/app/routing.py
@@ -4,5 +4,5 @@ from app.views import UserLookUpConsumer, DeviceRelationConsumer
 
 channel_routing = [
     route_class(UserLookUpConsumer, path=r'users$'),
-    route_class(DeviceRelationConsumer, path=r'device/(?P<from_device_id>[a-zA-Z0-9:-]+)$'),
+    route_class(DeviceRelationConsumer, path=r'device/(?P<from_device_id>[a-zA-Z0-9:- ]+)$'),
 ]
